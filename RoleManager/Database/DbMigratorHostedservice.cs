206224ca30c654c52988c95824e87afa0852ecad
Archery2000 <ongyuhann@hotmail.com>
2021-04-26 00:36:53 +0800

    add logging

--- RoleManager/Database/DbMigratorHostedservice.cs
+++ RoleManager/Database/DbMigratorHostedservice.cs
@@ -11,20 +11,21 @@ namespace RoleManager.Database
     public class DbMigratorHostedService : IHostedService
     {
         private readonly IServiceProvider _serviceProvider;
-        //private readonly IHostApplicationLifetime _lifetime;
+        private readonly IHostApplicationLifetime _lifetime;
         private readonly SourcedLoggingService _logging;
 
         public DbMigratorHostedService(IServiceProvider serviceProvider,
-            ILoggingService logger)
+            IHostApplicationLifetime lifetime, ILoggingService logger)
         {
+            Console.WriteLine("Initialising hosted service");
             _serviceProvider = serviceProvider;
-            //_lifetime = lifetime;
+            _lifetime = lifetime;
             _logging = new SourcedLoggingService(logger, "dbmigrator");
         }
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            
+            Console.WriteLine("start async");
             using var scope = _serviceProvider.CreateScope();
             try
             {
@@ -38,7 +39,7 @@ namespace RoleManager.Database
                     {
                         _logging.Fatal("Failed to contact DB, exiting application");
                         Environment.ExitCode = 1;
-                        //_lifetime.StopApplication();
+                        _lifetime.StopApplication();
                         break;
                     }
 
@@ -65,7 +66,7 @@ namespace RoleManager.Database
                 _logging.Fatal($"Database migration failed: {e.Message}");
                 _logging.Fatal(e.StackTrace);
                 Environment.ExitCode = 1;
-                //_lifetime.StopApplication();
+                _lifetime.StopApplication();
             }
         }
 
